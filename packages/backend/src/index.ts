f0bb7e60533b582142cedad3c9ec238ca9564b2d
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2024-12-29 20:02:22 +0100

    Added redirect on authorize endpoint.

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,10 @@
+import { authorizeController } from './controllers';
 import express from 'express';
 
 const app = express();
 const port = 3000;
+
+app.use('/', authorizeController.getRoutes());
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
