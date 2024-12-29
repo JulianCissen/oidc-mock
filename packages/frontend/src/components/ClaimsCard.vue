67cb446b993295eda93f5b38f518023feefdd649
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2024-12-29 22:32:48 +0100

    Added basic login functionality.

---
+++ packages/frontend/src/components/ClaimsCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <q-card class="custom-card">
+        <q-card-section>
+            <div class="text-h6">sub: {{ claims['sub'] }}</div>
+        </q-card-section>
+        <q-card-section>
+            <q-list>
+                <q-item
+                    v-for="[key, value] in Object.entries(claims)"
+                    :key="key"
+                >
+                    <q-item-section>
+                        <q-item-label>{{ key }}</q-item-label>
+                        <q-item-label caption>{{
+                            JSON.stringify(value, null, 2)
+                        }}</q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-list>
+        </q-card-section>
+        <q-card-actions align="right">
+            <filled-button label="Use claims" @click="useClaims" />
+        </q-card-actions>
+    </q-card>
+</template>
+
+<script setup lang="ts">
+import FilledButton from './FilledButton.vue';
+import { defineProps } from 'vue';
+
+type Props = {
+    claims: Record<string, unknown>;
+};
+defineProps<Props>();
+
+const emit = defineEmits(['use-claims']);
+const useClaims = () => {
+    emit('use-claims');
+};
+</script>
+
+<style scoped lang="scss">
+.custom-card {
+    width: 100%;
+}
+</style>
