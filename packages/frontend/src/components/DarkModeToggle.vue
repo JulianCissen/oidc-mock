de8dcae60b0d24ae4303523ad9ecb9d1c63d46c4
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-01-05 22:06:06 +0100

    Moved dark mode toggle to it's own component.

---
+++ packages/frontend/src/components/DarkModeToggle.vue
@@ -0,0 +1,25 @@
+<template>
+    <q-toggle
+        v-model="darkMode"
+        checked-icon="sym_o_dark_mode"
+        color="dark"
+        unchecked-icon="sym_o_light_mode"
+    />
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useQuasar } from 'quasar';
+
+const quasar = useQuasar();
+
+// Initialize darkMode with the value from localStorage.
+const darkMode = ref(window.localStorage.getItem('darkMode') === 'true');
+// Update dark mode setting based on the value of darkMode.
+watch(darkMode, (value) => {
+    quasar.dark.set(value);
+    window.localStorage.setItem('darkMode', value.toString());
+});
+// Initialize dark mode setting for Quasar.
+quasar.dark.set(darkMode.value ?? false);
+</script>
--- packages/frontend/src/layouts/MainLayout.vue
+++ packages/frontend/src/layouts/MainLayout.vue
@@ -3,12 +3,7 @@
         <q-header class="bg-primary text-white" elevated>
             <q-toolbar>
                 <q-toolbar-title> OIDC Mock Provider </q-toolbar-title>
-                <q-toggle
-                    v-model="darkMode"
-                    checked-icon="sym_o_dark_mode"
-                    color="dark"
-                    unchecked-icon="sym_o_light_mode"
-                />
+                <dark-mode-toggle />
                 <q-btn
                     v-if="authenticationStore.isAuthenticated"
                     dense
@@ -34,17 +29,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import DarkModeToggle from 'src/components/DarkModeToggle.vue';
 import { useAuthenticationStore } from 'src/stores/authentication';
-import { useQuasar } from 'quasar';
 
 const authenticationStore = useAuthenticationStore();
-const quasar = useQuasar();
-
-const darkMode = ref(window.localStorage.getItem('darkMode') === 'true');
-quasar.dark.set(darkMode.value);
-watch(darkMode, (value) => {
-    quasar.dark.set(value);
-    window.localStorage.setItem('darkMode', value.toString());
-});
 </script>
