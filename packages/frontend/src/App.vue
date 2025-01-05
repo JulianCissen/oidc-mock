d438562a5f1685a3c85ffb8e3655332cda118599
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-01-05 22:55:34 +0100

    Moved some dark mode logic around.

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -3,5 +3,8 @@
 </template>
 
 <script setup lang="ts">
-//
+import { useQuasar } from 'quasar';
+
+const quasar = useQuasar();
+quasar.dark.set(window.localStorage.getItem('darkMode') === 'true');
 </script>
