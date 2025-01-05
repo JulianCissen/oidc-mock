6b5b67821e07ce16a52540138fb4e2fcc7fd89ba
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-01-05 22:19:20 +0100

    Changed loader to circular indeterminate style. Removed placeholder text for consent route.

---
+++ packages/frontend/src/components/IndeterminateCircularProgressIndicator.vue
@@ -0,0 +1,8 @@
+<template>
+    <q-circular-progress
+        color="primary"
+        font-size="4px"
+        indeterminate
+        size="40px"
+    />
+</template>
--- packages/frontend/src/pages/ConsentPage.vue
+++ packages/frontend/src/pages/ConsentPage.vue
@@ -1,8 +1,11 @@
 <template>
-    <span>placeholder</span>
+    <q-page class="flex flex-center">
+        <indeterminate-circular-progress-indicator />
+    </q-page>
 </template>
 
 <script setup lang="ts">
+import IndeterminateCircularProgressIndicator from 'src/components/IndeterminateCircularProgressIndicator.vue';
 import { grantConsent } from 'src/vendors/internal';
 
 grantConsent();
