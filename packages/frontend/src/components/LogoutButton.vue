91a455d9c9b83312833e9ff48fce72ad0dd7d5dc
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-01-05 22:10:17 +0100

    Moved logout button to it's own component.

---
+++ packages/frontend/src/components/LogoutButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <q-btn
+        v-if="authenticationStore.isAuthenticated"
+        dense
+        flat
+        icon="sym_o_logout"
+        round
+        @click="authenticationStore.logout()"
+    />
+</template>
+
+<script setup lang="ts">
+import { useAuthenticationStore } from 'src/stores/authentication';
+
+const authenticationStore = useAuthenticationStore();
+</script>
