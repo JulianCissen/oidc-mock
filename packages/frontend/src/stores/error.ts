33f914ef109e0c0099d38e61a2dc123211ff34d1
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-01-06 17:37:53 +0100

    Added error screen functionality.

---
+++ packages/frontend/src/stores/error.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia';
+
+enum ErrorTypes {
+    NONE = 'none',
+    NOT_FOUND = 'notFound',
+    GENERIC = 'generic',
+}
+
+export const useErrorStore = defineStore('error', {
+    state: () => ({
+        type: ErrorTypes.NONE,
+    }),
+
+    getters: {
+        errored: (state) => state.type !== ErrorTypes.NONE,
+        isNotFoundError: (state) => state.type === ErrorTypes.NOT_FOUND,
+    },
+
+    actions: {
+        setNotFound() {
+            this.type = ErrorTypes.NOT_FOUND;
+        },
+        setGenericError() {
+            this.type = ErrorTypes.GENERIC;
+        },
+        clearError() {
+            this.type = ErrorTypes.NONE;
+        },
+    },
+});
