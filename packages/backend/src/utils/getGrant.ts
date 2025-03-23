341c787a7b50ed2139bf9129a2832799422ead2b
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-03-23 09:14:46 +0100

    Isolated some code in the auth controller.

---
+++ packages/backend/src/utils/getGrant.ts
@@ -0,0 +1,35 @@
+import type { Grant } from 'oidc-provider';
+import type Provider from 'oidc-provider';
+
+/**
+ * Returns either an existing grant or a new one.
+ * @param oidcProvider The OIDC provider instance.
+ * @param existingGrantId The existing grant ID.
+ * @param accountId The account ID.
+ * @param params The request parameters.
+ * @returns The grant.
+ */
+export const getGrant = async (
+    oidcProvider: Provider,
+    existingGrantId: string | undefined,
+    accountId: string,
+    params: Record<string, unknown>,
+): Promise<Grant> => {
+    let grant: Grant | undefined;
+
+    // Create grant or use existing one
+    if (existingGrantId) {
+        const foundGrant = await oidcProvider.Grant.find(existingGrantId);
+        if (foundGrant) grant = foundGrant;
+    }
+
+    // Create new grant if none exists
+    if (!grant) {
+        grant = new oidcProvider.Grant({
+            accountId,
+            clientId: String(params['client_id']),
+        });
+    }
+
+    return grant;
+};
