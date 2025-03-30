f578b636de093655466005e62df806c19e10e501
Julian Cissen <107254512+JulianCissen@users.noreply.github.com>
2025-03-30 16:56:27 +0200

    feat: implement silent login functionality and enhance session management

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -3,8 +3,21 @@
 </template>
 
 <script setup lang="ts">
+import { useAuthenticationStore } from 'src/stores/authentication';
 import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
 
 const quasar = useQuasar();
 quasar.dark.set(window.localStorage.getItem('darkMode') === 'true');
+
+const authenticationStore = useAuthenticationStore();
+const router = useRouter();
+
+authenticationStore.initializeSession(() => {
+    // Navigate to callback if silent sign-in was successful
+    // Only if we're currently on the home page to avoid disrupting other workflows
+    if (router.currentRoute.value.path === '/') {
+        router.push('/callback');
+    }
+});
 </script>
--- packages/frontend/src/components/ClearSessionButton.vue
+++ packages/frontend/src/components/ClearSessionButton.vue
@@ -1,15 +1,14 @@
 <template>
     <icon-button
-        v-if="authenticationStore.isAuthenticated && isDevelopment"
-        icon="restart_alt"
-        tooltip-text="Clear session state (testing only)"
+        v-if="authenticationStore.isAuthenticated"
+        icon="no_accounts"
+        tooltip-text="Clear internal client session"
         @click="clearSession"
     />
 </template>
 
 <script setup lang="ts">
 import IconButton from 'src/components/IconButton.vue';
-import { computed } from 'vue';
 import { useAuthenticationStore } from 'src/stores/authentication';
 
 const authenticationStore = useAuthenticationStore();
@@ -18,6 +17,4 @@ const authenticationStore = useAuthenticationStore();
 const clearSession = async () => {
     await authenticationStore.clearSession();
 };
-// Check if we're in development mode.
-const isDevelopment = computed(() => process.env.NODE_ENV === 'development');
 </script>
